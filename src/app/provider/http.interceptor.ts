90a842ce7acbe430289359123fd434acc64483ba
Tayer <tayergustavo@hotmail.com>
2022-09-17 08:27:17 -0300

    corrigindo ip aplicaçao

--- src/app/provider/http.interceptor.ts
+++ src/app/provider/http.interceptor.ts
@@ -13,9 +13,8 @@ export class HttpInterceptorService implements HttpInterceptor {
   constructor() {}
 
   intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const BASE_URL: string = 'http://100.25.248.37:8080'
+    const BASE_URL: string = 'http://54.243.29.48:8080'
     var token = localStorage.getItem('token');
-    console.log(token)
     const apiReq = req.clone({
       url: `${BASE_URL}/${req.url}`,
       setHeaders: {
